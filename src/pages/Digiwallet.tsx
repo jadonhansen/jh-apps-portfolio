f77ef3498bfc1c259857dfca3285553939f5949b
Jadon Hansen <jadonhansen@gmail.com>
2022-10-23 17:51:27 +0200

    wip weatherly and digiwallet page

--- src/pages/Digiwallet.tsx
+++ src/pages/Digiwallet.tsx
@@ -7,27 +7,74 @@ export default function Digiwallet() {
 
 	return (
 		<div className="container-md digiwallet">
-			<h4>DigiWallet home page coming soon!</h4>
-			<p>
-				Click <Link to="privacy_policy">here</Link> to view Privacy Policy
-			</p>
-			<div className="badges">
-				<a
-					href="https://play.google.com/store/apps/details?id=com.digiwalletapp.digiwallet&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img className="androidBadge" id="findMe" alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" />
-				</a>
+			<div className="row landing">
+				<div className="col-md-8">
+					<div className="app-title">
+						<img src={require("../assets/digiwallet/AppIcon.png")}></img>
+						<h1>Digiwallet</h1>
+					</div>
+					<p>For an effortless shopping experience.</p>
+					<div className="badges">
+						<a href="https://apps.apple.com/us/app/digiwallet/id1593438301?itsct=apps_box_badge&amp;itscg=30200"
+							className="iosBadge"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1638230400&h=5e365ba6b8b5c634c223f9a9e3bff004"
+								alt="Download on the App Store" style={{borderRadius: "13px", width: "250px", height: "83px"}} />
+						</a>
 
-				<a href="https://apps.apple.com/us/app/digiwallet/id1593438301?itsct=apps_box_badge&amp;itscg=30200"
-					className="iosBadge"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1638230400&h=5e365ba6b8b5c634c223f9a9e3bff004"
-						alt="Download on the App Store" style={{borderRadius: "13px", width: "250px", height: "83px"}} />
-				</a>
+						<a
+							href="https://play.google.com/store/apps/details?id=com.digiwalletapp.digiwallet&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img className="androidBadge" id="findMe" alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" />
+						</a>
+					</div>
+				</div>
+				<div className="col-md-4">
+					<img className="landing-img" src={require("../assets/digiwallet/digiwalletLanding.png")}></img>
+				</div>
+			</div>
+
+			<div className="row about">
+				<div className="col-md-6">
+					<img src={require("../assets/digiwallet/digiwalletAbout.png")}></img>
+				</div>
+				<div className="col-md-6">
+					<h3>About</h3>
+					<p>With DigiWallet you&apos;re able to digitally store all of your barcoded loyalty, rewards or club cards.</p>
+					<p> Developed for an effortless experience - you&apos;re able to add, edit, find and display your cards all while you&apos;re in line at the till, it&apos;s that quick!</p>
+					<p>We&apos;ve made it possible to choose from an ever expanding range of brands or by creating cards of your own. If you have any cards you would love to see, feel free to let us know by using the brand suggestion feature in the app.</p>
+					<p>DigiWallet was designed to ease the shopping experience, a problem we have all faced at one point.</p>
+				</div>
+			</div>
+
+			<div className="row features">
+				<h3>Features</h3>
+				<div className="col-md-6">
+					<img src={require("../assets/digiwallet/digiwalletFeatures.png")}></img>
+				</div>
+				<div className="col-md-6">
+					<p>Add any card with a barcode for scanning.</p>
+					<p>Pick from well-known card brands or create your own custom cards.</p>
+					<p>Easily find and organise the way you view your cards.</p>
+					<p>Dark theme and light theme.</p>
+				</div>
+			</div>
+
+			<div className="row">
+				<h3>Reviews from users</h3>
+				<div className="review-box">
+
+				</div>
+				<div className="review-box">
+
+				</div>
+				<div className="review-box">
+
+				</div>
 			</div>
 		</div>
 	);
--- src/pages/Weatherly.tsx
+++ src/pages/Weatherly.tsx
@@ -7,26 +7,90 @@ export default function Weatherly() {
 
 	return (
 		<div className="container-md weatherly">
-			<h4>Weatherly home page coming soon!</h4>
-			<p>
-				Click <Link to="privacy_policy">here</Link> to view Privacy Policy
-			</p>
-			<div className="badges">
-				<a
-					href="https://play.google.com/store/apps/details?id=com.jadonhansen.weatherly&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img className="androidBadge" id="findMe" alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" />
-				</a>
-				<a href="https://apps.apple.com/za/app/weatherly/id1583456822?itsct=apps_box_badge&amp;itscg=30200"
-					className="iosBadge"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1632096000&h=c1b7a961cd4d624bc76116e140473847"
-						alt="Download on the App Store" style={{borderRadius: "13px", width: "250px", height: "83px"}} />
-				</a>
+			<div className="row landing">
+				<div className="col-md-8">
+					<div className="app-title">
+						<img src={require("../assets/weatherly/AppIcon.png")}></img>
+						<h1>Weatherly</h1>
+					</div>
+					<p>Bringing you the best of weather, wherever, whenever.</p>
+					<div className="badges">
+						<a href="https://apps.apple.com/za/app/weatherly/id1583456822?itsct=apps_box_badge&amp;itscg=30200"
+							className="iosBadge"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1632096000&h=c1b7a961cd4d624bc76116e140473847"
+								alt="Download on the App Store" style={{borderRadius: "13px", width: "250px", height: "83px"}} />
+						</a>
+						<a
+							href="https://play.google.com/store/apps/details?id=com.jadonhansen.weatherly&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img className="androidBadge" id="findMe" alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" />
+						</a>
+					</div>
+				</div>
+				<div className="col-md-4">
+					<img className="landing-img" src={require("../assets/weatherly/weatherlyLanding.png")}></img>
+				</div>
+			</div>
+
+			<div className="row about">
+				<div className="col-md-6">
+					<img src={require("../assets/weatherly/weatherlyAbout.png")}></img>
+				</div>
+				<div className="col-md-6">
+					<h3>About</h3>
+					<p>Weatherly was designed for the user who doesn&apos;t have time to interpret complicated graphs and weather statistics.</p>
+					<p>With screen time so high nowadays Weatherly makes sure you get your weather related answers as easily as possible and in no time at all.</p>
+					<p>Easily personalise your experience with light or dark mode, imperial or metric unit settings and much more!</p>
+				</div>
+			</div>
+
+			<div className="row features">
+				<div className="col-md-4">
+					<h3>Features</h3>
+					<p>- Your current location&apos;s weather which updates as you move about.</p>
+					<p>- A 24 hour and 7 day forecast for your location.</p>
+					<p>- Weather inaccuracy reporting.</p>
+					<p>- Search capabilities for any location across the globe with an interactive map and text search.</p>
+					<p>- Save searched locations to your personal list, accurate to its&apos; timezone.</p>
+					<p>- Customisable wind speed units, temperature units, time format, rain units and much more!</p>
+					<p>- Sunrise and sunset time.</p>
+					<p>- Moon phases.</p>
+					<p>- Dark theme & light theme.</p>
+				</div>
+				<div className="col-md-4">
+					<img src={require("../assets/weatherly/weatherlyFeatures.png")}></img>
+				</div>
+				<div className="col-md-4">
+					<h4>Premium features</h4>
+					<p>- 24 hour and 7 day forecast graphs.</p>
+					<p>- Save an infinite amount of locations.</p>
+					<p>- Set a saved location to be your default home screen location.</p>
+					<p>- No ads.</p>
+				</div>
+			</div>
+
+			<div className="row">
+				<h3>API Source</h3>
+				<p>The weather data originates from the global weather API provider OpenWeatherMap, providing you with pinpoint accuracy data. Don&apos;t agree? Then utilise our weather-accuracy feature to help us improve the data.</p>
+				<a href="https://openweathermap.org/guide" target="_blank" rel="noreferrer">OpenWeatherMap</a>
+			</div>
+
+			<div className="row">
+				<h3>Reviews from users</h3>
+				<div className="review-box">
+
+				</div>
+				<div className="review-box">
+
+				</div>
+				<div className="review-box">
+
+				</div>
 			</div>
 		</div>
 	);
